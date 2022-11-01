e0f3936449d53546b0b5985cc532d1954018a7f0
DamianJavierAguirreCerullo <djac441@gmail.com>
2022-11-01 19:13:46 -0300

    Se pone rojo si no esta bien el email o la pass

--- frontend/src/assets/components/Modal.jsx
+++ frontend/src/assets/components/Modal.jsx
@@ -6,8 +6,8 @@ export const Modal = () => {
 		<div className='modal'>
 			<div className='modal__container'>
 			<h3>Iniciar Sesión</h3>
-			<input className="inputs__login" type="emailt" placeholder='Escriba su correo electrónico' />
-			<input className="inputs__login" type="password" placeholder='Escriba su contraseña' />
+			<input required className="inputs__login" type="email" placeholder='Escriba su correo electrónico' />
+			<input required className="inputs__login" type="password" minlength="6" placeholder='Escriba su contraseña' />
 			<button className='modal__button'>Entrar</button>
 			<p><strong>¿No tienes una cuenta?</strong> <a href="#">Regístrate</a></p>
 			</div>
