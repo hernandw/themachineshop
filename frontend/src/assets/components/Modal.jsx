41549946601c309822a2bd32b2176b9a3b88cd66
rumer tovar <rumertovar7474@gmail.com>
2022-11-04 13:34:34 -0500

    login feature

--- frontend/src/assets/components/Modal.jsx
+++ frontend/src/assets/components/Modal.jsx
@@ -1,16 +1,62 @@
-import React from 'react'
-import cerrarModal from '../icons/cerrar.svg'
+import React, { useState } from 'react';
+import { useLoginForm } from '../hooks/useLoginForm';
+import cerrarModal from '../icons/cerrar.svg';
+import Input from './login/Input';
 
 export const Modal = () => {
-	return (
-		<div className='modal'>
-			<div className='modal__container'>
-			<h3>Iniciar Sesión</h3>
-			<input required className="inputs__login" type="email" placeholder='Escriba su correo electrónico' />
-			<input required className="inputs__login" type="password" minlength="6" placeholder='Escriba su contraseña' />
-			<button className='modal__button'>Entrar</button>
-			<p><strong>¿No tienes una cuenta?</strong> <a href="#">Regístrate</a></p>
-			</div>
-		</div>
-	)
-}
+ const {
+  form,
+  errors,
+  handleChange,
+  handleBlur,
+  handleSubmit,
+  loading,
+  responseMessage,
+ } = useLoginForm();
+
+ return (
+  <div className='modal'>
+   <form onSubmit={handleSubmit} className='modal__container'>
+    <h3>Iniciar Sesión</h3>
+
+    <Input
+     name='email'
+     className='inputs__login'
+     type='text'
+     placeholder='Escriba su correo electrónico'
+     autoComplete='on'
+     onChange={handleChange}
+     onBlur={handleBlur}
+     value={form.email}
+     errors={errors}
+    />
+    <Input
+     name='password'
+     className='inputs__login'
+     type='password'
+     placeholder='Escriba su contraseña'
+     autoComplete='on'
+     onChange={handleChange}
+     onBlur={handleBlur}
+     value={form.password}
+     errors={errors}
+    />
+    {responseMessage ? responseMessage : ''}
+    {!loading ? (
+     <input type='submit' className='modal__button' value='Entrar' />
+    ) : (
+     <div className='lds-ring'>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+     </div>
+    )}
+
+    <p>
+     <strong>¿No tienes una cuenta?</strong> <a href='#'>Regístrate</a>
+    </p>
+   </form>
+  </div>
+ );
+};
