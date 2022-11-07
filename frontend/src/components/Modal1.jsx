3892360e665859613484346cc724da227320e3a2
Williams Hernandez <hernandw@gmail.com>
2022-11-07 12:14:02 -0300

    :bug: corrección de rutas

---
+++ frontend/src/components/Modal1.jsx
@@ -0,0 +1,41 @@
+import cerrarBTN from "../assets/icons/cerrar.svg";
+
+export const Modal = ({ setModal, animarModal, setAnimarModal }) => {
+  const onHandle = () => {
+		setAnimarModal(false)
+		
+		setTimeout(()=>{
+			
+			setModal(false)
+		}, 500)
+  };
+
+  return (
+    <div className="modal">
+      <div className="cerrar-modal">
+        <img src={cerrarBTN} alt="cerrar modal" onClick={onHandle} />
+      </div>
+
+			<form className={`formulario ${animarModal ? 'animar' : 'cerrar'}`}>
+				<legend className="login__title">Iniciar Sesión</legend>
+				<input
+          required
+          className="inputs__login"
+          type="email"
+          placeholder="Escriba su correo electrónico"
+        />
+				 <input
+          required
+          className="inputs__login"
+          type="password"
+          placeholder="Escriba su contraseña"
+        />
+				<button className="modal__button">Entrar</button>
+				<p>
+          <strong>¿No tienes una cuenta?</strong> <a href="/register">Regístrate</a>
+        </p>
+			</form>
+
+    </div>
+  );
+};
