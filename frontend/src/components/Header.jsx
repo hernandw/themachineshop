b707198265abc41cbcf0c5c975b979b986a57562
Rumer Tovar <rumertovar7474@gmail.com>
2022-11-09 16:58:40 -0500

    add to cart and clear cart

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -7,9 +7,8 @@ import { useNavigate } from 'react-router-dom';
 import { useContext } from 'react';
 import CartContext from '../context/CartContext';
 export const Header = ({ setModal, setAnimarModal, user, setUser }) => {
- const { products } = useContext(CartContext);
+ const { cart } = useContext(CartContext);
 
- console.log(products);
  const navigate = useNavigate();
 
  const onHandleModal = () => {
--- frontend/src/pages/Cart.jsx
+++ frontend/src/pages/Cart.jsx
@@ -1,29 +1,51 @@
-import React from 'react';
-import productImage from '../assets/images/lampara.jpeg';
-
+import { useContext } from 'react';
+import { TYPES } from '../actions/cartActions';
+import CartContext from '../context/CartContext';
+CartContext;
 export function Cart() {
+ const { dispatch, cart } = useContext(CartContext);
+
+ const addToCart = (id) => {
+  dispatch({ type: TYPES.ADD_TO_CART, payload: id });
+ };
+
+ const delFromCart = (id, all = false) => {
+  if (all) {
+   dispatch({ type: TYPES.REMOVE_ALL_FROM_CART, payload: id });
+  } else {
+   dispatch({ type: TYPES.REMOVE_ONE_FROM_CART, payload: id });
+  }
+ };
+
+ const clearCart = () => {
+  dispatch({ type: TYPES.CLEAR_CART });
+ };
+
+ const formateado = (number) => {
+  return number.toLocaleString('ES-es', {
+   style: 'currency',
+   currency: 'USD',
+  });
+ };
  return (
-  <div>
-   <h1>Carrito</h1>
-   <div className=''>
-    <div className='card'>
-     <div>
-      <img src={productImage} />
-      <p className='card__title'></p>
-      <p className='card__price'>132</p>
-      <button className='card__button'>Ver producto</button>
+  <>
+   <h3>Carrito</h3>
+   <h2>
+    Total: ${cart.reduce((acc, item) => acc + item.price * item.quantity, 0)}.00
+   </h2>
+   <article className='box'>
+    <button onClick={clearCart}>Limpiar Carrito</button>
+    {cart.map((product) => (
+     <div className='card' key={product.id}>
+      <div>
+       <img src={product.img} />
+       <p className='card__title'>{product.name}</p>
+       <p className='card__price'>{formateado(product.price)}</p>
+       <p>cantidad: {product.quantity}</p>
+      </div>
      </div>
-    </div>
-    <div className='card'>
-     <div>
-      <img src={productImage} />
-      <p className='card__title'>asd</p>
-      <p className='card__price'>132</p>
-      <button className='card__button'>Ver producto</button>
-     </div>
-    </div>
-   </div>
-   <h2>subtotal</h2>
-  </div>
+    ))}
+   </article>
+  </>
  );
 }
