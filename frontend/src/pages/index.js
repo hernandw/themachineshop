ddbbc1466cfd79fc4265dc49616d994c1c6cf5f3
Williams Hernandez <hernandw@gmail.com>
2022-11-10 11:20:22 -0300

    :bug: merge conflict

--- frontend/src/pages/index.js
+++ frontend/src/pages/index.js
@@ -1,11 +1,3 @@
-<<<<<<< HEAD
-export * from "./About";
-export * from "./Answers";
-export * from "./Contact";
-export * from "./Privacy";
-export * from "./Register";
-export * from "./Work";
-=======
 export * from './About';
 export * from './Answers';
 export * from './Contact';
@@ -13,4 +5,3 @@ export * from './Privacy';
 export * from './Register';
 export * from './Work';
 export * from './Cart';
->>>>>>> 1a949805bc18bc0616ce2e921aa20156e17ae316
