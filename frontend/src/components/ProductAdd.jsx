4f61b09aed3dcb6c6cce7684c2e9ce00cb4ab542
Williams Hernandez <hernandw@gmail.com>
2022-11-16 05:51:44 -0300

    conexión de productos con el backend

--- frontend/src/components/ProductAdd.jsx
+++ frontend/src/components/ProductAdd.jsx
@@ -1,14 +1,15 @@
-import { useLoaderData } from 'react-router-dom'
-import { getProducts } from '../data/GetProducts';
+import { useLoaderData } from "react-router-dom";
+import { getProducts } from "../data/GetProducts";
 
-
-export const loaderProductos = ()=>{
-	const productsAll = getProducts()
-	return productsAll
-} 
+export const loaderProductos = () => {
+	const productsAll = getProducts();
+	return productsAll;
+};
 
 export const ProductAdd = () => {
 	const productsAll = useLoaderData();
+	console.log(productsAll);
+
 	return (
 		<>
 			<h3 className='title__admin'>Administración de Productos</h3>
@@ -19,27 +20,36 @@ export const ProductAdd = () => {
 							<th className='img'>Imagen</th>
 							<th className='name'>Nombre</th>
 							<th className='price'>Precio</th>
+							<th>Creado</th>
+							<th>Categoria</th>
+							<th>cantidad</th>
+							<th>Descripcion</th>
 							<th>Acción</th>
 						</tr>
 					</thead>
 					<tbody>
-						{
-							productsAll.map(producto =>(
-								<tr key={producto.id}>
-									<td><img className='products__img' src={producto.imagen} alt={producto.name} /></td>
-									<td>{producto.name}</td>
-									<td>{producto.price}</td>
-									<td>
-				<button className="buttonEdit">Editar</button>
-				<button className="buttonDelete">Eliminar</button>
-			</td>
-								</tr>
-							))
-						}
+						{productsAll.map((p) => (
+							<tr key={p.id_product}>
+								<td>
+									<img className='products__img' src={p.product_image_url} alt='' />
+								</td>
+								<td>{p.product_name}</td>
+								<td>{p.product_price}</td>
+								<td>{p.product_creation_date}</td>
+								<td>{p.id_category}</td>
+								<td>{p.product_quantity}</td>
+								<td>{p.product_description}</td>
+
+								<td>
+									<button className='buttonEdit'>Editar</button>
+									<button className='buttonDelete'>Eliminar</button>
+								</td>
+							</tr>
+						))}
 					</tbody>
 				</table>
 			) : (
-				<p>No hay usuarios aún</p>
+				<p>No hay productos aún</p>
 			)}
 		</>
 	);
--- frontend/src/data/GetProducts.js
+++ frontend/src/data/GetProducts.js
@@ -1,6 +1,6 @@
 export const getProducts = async () => {
 	const url =  import.meta.env.VITE_API_URL;
-	const respuesta = await fetch(`${url}/products`)
+	const respuesta = await fetch(`${url}/api/products`)
 	const resultado = respuesta.json()
 	return resultado
 };
